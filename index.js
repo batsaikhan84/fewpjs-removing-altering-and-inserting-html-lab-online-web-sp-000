3a2da49b91a1ee580965a4d8a768b2bfc1207699
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-07-30 17:33:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+main.remove()
+
+
+
+
+let newHeader = document.createElement('h1');
+newHeader.appendChild('victory')
+
+document.body.appendChild(newHeader)
+
+let newHeader = document.querySelector("h1#")
