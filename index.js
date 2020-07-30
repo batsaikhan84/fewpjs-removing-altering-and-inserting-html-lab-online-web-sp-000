d7403dc250926a225548377245072301cc8078d1
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-07-30 17:34:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 main.remove()
 
-
-
-
 let newHeader = document.createElement('h1');
-newHeader.appendChild('victory')
-
-document.body.appendChild(newHeader)
-
-let newHeader = document.querySelector("h1#")
+newHeader.setAttribute('id', 'victory');
+let newHeader = document.querySelector('h1#victory');
+newHeader.innerHTML = 'Batsaikhan is the champion';
